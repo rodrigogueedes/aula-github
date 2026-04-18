b93c4396716f82876b1a0c675f7e1a5329c6beee
RodrigooGuedes <rodrigogueedes7@gmail.com>
2026-04-18 10:31:14 -0300

    Criação do arquivo readme

--- OlaMundo.java
+++ OlaMundo.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class OlaMundo {
     public static void main(String[] args) {
-        System.out.println("Olá, mundo");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.printf("Olá mundo, %s", nome );
+
+        scanner.close();
+
     }
 }
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Aula de Git e GitHub
+
+## Comandos Git
+- `git --version`: Retorna a versão do Git instalado.
+- `git clone URL`: Cria uma cópia local de um repositório remoto.
+- `git add .` : Adiciona os arquivos/modificações realizados no próximo commit.
+- `git commit -m  "..."`: Criar o commit com uma mensagem descritiva
